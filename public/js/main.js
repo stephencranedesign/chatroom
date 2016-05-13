4bbaaf394ace163bbfb81f2f2151c0f135b28657
Stephen Crane <stephenrusselcrane@gmail.com>
2016-05-13 01:52:12 +0000

    enhancing chat room

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,45 @@
 $(document).ready(function() {
-	var input = $('input');
+	var socket = io.connect('https://chat-room-stephenrusselcrane.c9users.io');
+	var input = $('#msg');
 	var messages = $('#messages');
-
-	var addMessage = function(message) {
-		messages.append('<div>'+message+'</div>');
+	var users = $('#users');
+	
+	var privateMsg = $('#privateMsg');
+	var privateAud = $('#private-audience');
+	var sendMsg = $('#send-private-message');
+	
+	sendMsg.on('click', function() {
+		var audience = privateAud.val();
+		var val = privateMsg.val();
+		
+		var msg = new Message(val, 'private', audience);
+		prevMessages.push(msg);
+		addMessage(msg);
+		socket.emit('message::private', msg);
+		privateAud.val('');
+		privateMsg.val('');
+		
+	});
+	
+	var Message = function(msg, type, audience) {
+		this.time = new Date().toLocaleString();
+		this.id = id;
+		this.msg = msg;
+		this.type = type || 'msg';
+		this.audience = audience;
+	};
+	
+	var id;
+	
+	var prevMessages = [];
+	
+	var buildMessage = function(o) {
+		if(o.type === 'notice') return '<div class="msg"><div div="msg-meta"><div class="msg-author">from: '+o.id+' | </div> <div class="msg-date">date: '+o.time+' | </div><div class="msg-notice"> '+ o.msg +' </div></div></div>';
+		return '<div class="msg"><div div="msg-meta"><div class="msg-author">from: '+o.id+' | </div> <div class="msg-date">date: '+o.time+'</div></div>'+ o.msg +'</div>';
+	};
+	var addMessage = function(o) {
+		prevMessages.push(o);
+		messages.append(buildMessage(o));
 	};
 
 	input.on('keydown', function(event) {
@@ -11,9 +47,58 @@ $(document).ready(function() {
 			return;
 		}
 
-		var message = input.val();
-		addMessage(message);
+		var val = input.val();
+		var msg = new Message(val);
+		prevMessages.push(msg);
+		addMessage(msg);
+		socket.emit('message', msg);
 		input.val('');
 	});
 	
+	input.on('focus', function(e) {
+		e.target.className = e.target.className + ' focus';
+		socket.emit('userTyping', { id: id });
+	});
+	
+	socket.on('message', function(o) { prevMessages.push(o); addMessage(o); });
+	socket.on('connected', function(o) { 
+		users.html('totalUsers: ' + o.totalUsers);
+	});
+	socket.on('disconnected', function(o) {
+		var msg = new Message('user exit', 'notice');
+		prevMessages.push(msg);
+		addMessage(msg); 
+		users.html('totalUsers: ' +o.totalUsers);
+	});
+	
+	socket.on('bootstrap', function(o) {
+		console.log('bootstrap: ', o);
+		prevMessages = o.prevChatSession;
+		o.prevChatSession.forEach(function(msg) {
+			addMessage(msg);
+		});
+		id = o.id;
+	});
+	
+	socket.on('nickname::changed', function(o) {
+		prevMessages = o;	
+		var array = [];
+		prevMessages.forEach(function(msg) {
+			array.push(buildMessage(msg));
+		});
+		messages.html(array.join(''));
+	});
+	
+	/* nickname */
+	var nickname = $('#nickname');
+	nickname.on('keydown', function() {
+		if(event.keyCode != 13) {
+			return;
+		}
+		
+		var val = nickname.val();
+		socket.emit('nickname::change', { id: id, nickname: val });
+		id = val;
+		nickname.val('');
+	});
 });
--- server.js
+++ server.js
@@ -8,13 +8,68 @@ app.use(express.static('public'));
 var server = http.Server(app);
 var io = socket_io(server);
 
+var prevChatSession = [];
+var totalUsers = 0;
+
+var uid = 1;
+
+var users = {};
+
+var User = function(socket) {
+	this.id = uid;
+	uid++;
+	this.socket = socket;
+	users[this.id] = this;
+};
+
+function timeStamp() { return new Date().toLocaleString() }
+
 io.on('connection', function(socket) {
 	console.log('Client connected');
-
-	socket.io('message', function(message) {
+	
+	var user = new User(socket);
+	
+	prevChatSession.push({ id: user.id, time: timeStamp(), msg: 'user entered', type: 'notice' });
+	totalUsers++;
+	
+	io.emit('connected', { totalUsers: totalUsers, id: user.id }); // send to all
+	socket.broadcast.emit('message', { id: user.id, time: timeStamp(), msg: 'user entered', type: 'notice' }); // all but this socket
+	socket.emit('bootstrap', { prevChatSession: prevChatSession, id: user.id }); // only this socket
+	
+	socket.on('message', function(message) {
 		console.log('Received message: ', message);
+		prevChatSession.push(message);
+		socket.broadcast.emit('message', message);
 	});
+	
+	socket.on('disconnect', function() {
+		totalUsers--;
+		prevChatSession.push({ id: user.id, time: timeStamp(), msg: 'user exit', type: 'notice' });
+		io.emit('disconnected', { totalUsers: totalUsers, id: user.id });
+		
+		delete users[user.id];
+	});
+	
+	socket.on('nickname::change', function(o) {
+		updateNickname(o.id, o.nickname);
+		io.emit('nickname::changed', prevChatSession);
+	});
+	
+	socket.on('message::private', function(msg) {
+		console.log('Received message: ', msg);
+		
+		var privateSocket = users[msg.audience].socket;
+		privateSocket.emit('message', msg);
+	});
+
 });
 
+function updateNickname(id, val) {
+	console.log('updateNickname: ', id, val);
+	prevChatSession.forEach(function(msg) {
+		if(msg.id === id) msg.id = val;
+	});
+}
+
 server.listen(8080);
-console.log('listening at :8080');
+console.log('server listening at :8080');
